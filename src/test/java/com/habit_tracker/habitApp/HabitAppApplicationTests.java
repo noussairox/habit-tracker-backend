62cc8b2c8d716413f8043ae62c9055f4664813a0
noussairox <abellouch34@gmail.com>
2024-11-06 18:41:00 +0100

    Initial project setup

---
+++ src/test/java/com/habit_tracker/habitApp/HabitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habit_tracker.habitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
